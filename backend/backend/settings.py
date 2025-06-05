ef716a69514f84f29d10309fdde99e3e8d49ac2d
Niyaz Ahamad Herkal <niyaz47nhh@gmail.com>
2025-06-05 23:39:59 +0530

    reverted old code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,8 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-soidas'djsdas
- wda wdka
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
